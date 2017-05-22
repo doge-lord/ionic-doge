ed0563c3318d362efa4aeb776c506b558e29f517
jan.calayag <jannoel.calayag@gmail.com>
2017-05-22 16:59:15 +0800

    Helper text feature (Initial commit)

---
+++ src/components/forms/helper-text/helper-text.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'ion-doge-helper-text',
+    templateUrl: 'helper-text.component.html',
+})
+export class HelperText {
+
+}
--- src/components/ionic-doge.module.ts
+++ src/components/ionic-doge.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignaturePadModule } from 'angular2-signaturepad';
 
+import { HelperText } from './forms/helper-text/helper-text.component';
 import { SignatureField } from './forms/signature-field/signature-field.component';
 
 @NgModule({
   declarations: [
+    HelperText,
     SignatureField
   ],
   imports: [
@@ -15,6 +17,7 @@ import { SignatureField } from './forms/signature-field/signature-field.componen
     SignaturePadModule
   ],
   exports: [
+    HelperText,
     SignatureField
   ]
 })
